8e3a08edb59e8235961ee91b8bde9723b69cf00d
Ibai Imaz <ibaiimaz@gmail.com>
2021-01-14 12:01:30 +0100

    Is a leap year if divisible by 100 but also by 400

--- src/LeapYearChecker.js
+++ src/LeapYearChecker.js
@@ -1,7 +1,7 @@
 class LeapYearChecker {
 
     isLeap(year) {
-        if (year % 4 !== 0 || year % 100 === 0) {
+        if (year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0)) {
             return false;
         }
         return true;
--- test/LeapYearChecker.test.js
+++ test/LeapYearChecker.test.js
@@ -18,4 +18,8 @@ describe('LeapYearChecker', () => {
     it('should determine that is not a leap year if divisible by 100', () => {
         expect(leapYearChecker.isLeap(1900)).to.equal(false);
     });
+
+    it('should determine that is a leap year if divisible by 100 but also by 400', () => {
+        expect(leapYearChecker.isLeap(2000)).to.equal(true);
+    });
 });
