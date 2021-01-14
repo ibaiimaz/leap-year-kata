9a18326547d53e7fb2623926aa8bb04c1c1cc6f6
Ibai Imaz <ibaiimaz@gmail.com>
2021-01-14 12:05:20 +0100

    Invert conditional for better legibility

--- src/LeapYearChecker.js
+++ src/LeapYearChecker.js
@@ -1,10 +1,10 @@
 class LeapYearChecker {
 
     isLeap(year) {
-        if (year % 4 !== 0 || (year % 100 === 0 && year % 400 !== 0)) {
-            return false;
+        if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+            return true;
         }
-        return true;
+        return false;
     }
 }
 
