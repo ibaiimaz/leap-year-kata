c6f491f2fde22af99682bd0c08ce06dd9ec4752e
Ibai Imaz <ibaiimaz@gmail.com>
2021-01-14 08:35:34 +0100

    Refactor, extract class instantiation to beforeEach

--- test/LeapYearChecker.test.js
+++ test/LeapYearChecker.test.js
@@ -1,18 +1,21 @@
 const LeapYearChecker = require('../src/LeapYearChecker');
 
 describe('LeapYearChecker', () => {
+    let leapYearChecker;
+
+    beforeEach(() => {
+        leapYearChecker = new LeapYearChecker();
+    });
+
     it('should determine that is not a leap year if not divisible by 4', () => {
-        const leapYearChecker = new LeapYearChecker();
         expect(leapYearChecker.isLeap(2001)).to.equal(false);
     });
 
     it('should determine that is a leap year if divisible by 4', () => {
-        const leapYearChecker = new LeapYearChecker();
         expect(leapYearChecker.isLeap(1996)).to.equal(true);
     });
 
     it('should determine that is not a leap year if divisible by 100', () => {
-        const leapYearChecker = new LeapYearChecker();
         expect(leapYearChecker.isLeap(1900)).to.equal(false);
     });
 });
