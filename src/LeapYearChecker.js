8d0b3abeb80924a63feba6d8954dc7a90c37147b
Ibai Imaz <ibaiimaz@gmail.com>
2021-01-14 12:21:04 +0100

    Inline de conditional

--- src/LeapYearChecker.js
+++ src/LeapYearChecker.js
@@ -1,10 +1,8 @@
 class LeapYearChecker {
 
     isLeap(year) {
-        if (this._isDivisibleBy(4, year) && (!this._isDivisibleBy(100, year) || this._isDivisibleBy(400, year))) {
-            return true;
-        }
-        return false;
+        return this._isDivisibleBy(4, year) && 
+            (!this._isDivisibleBy(100, year) || this._isDivisibleBy(400, year));
     }
 
     _isDivisibleBy(divisor, year) {
